0a5ba9f4284daa4033b00627a288e4c74cd25de0
Mandvi Shukla <mandvishukla9116@gmail.com>
2026-06-06 23:23:17 +0530

    Level 4 JavaScript Functionality Completed

---
+++ script.js
@@ -0,0 +1,10 @@
+let cart = 0;
+
+function addToCart() {
+    cart++;
+
+    document.getElementById("cart-count").innerText = cart;
+}
+function validateForm() {
+    alert("Form Submitted Successfully!");
+}
